00409dffa2927726e6edc33bc7ef29fd3b00d882
Arish Ahmad <arishahmad661@gmail.com>
2026-05-02 15:21:48 +0530

    factory pattern & abstract factory

--- 4-factory-pattern.cpp
+++ 4-factory-pattern.cpp
@@ -1,79 +1,205 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
-class PizzaStore {
+class Dough{
     public:
-        virtual Pizza createPizza(string type) = 0;
-        Pizza orderPizza(string type){
-            Pizza pizza = createPizza(type);
-            pizza.prepare();
-            pizza.cut();
-            pizza.box();
-            return pizza;
+        virtual string getName() = 0;
+        virtual ~Dough() = default;
+};
+
+class ThickCrustDough : public Dough{
+    public:
+        string getName() override { return "Thick Crust Dough";}
+};
+
+class ThinCrustDough : public Dough{
+    public:
+        string getName() override { return "Thin Crust Dough";}
+};
+
+
+class Sauce{
+    public:
+        virtual string getName() = 0;
+        virtual ~Sauce() = default;
+};
+
+class MarinaraSauce : public Sauce{
+    public:
+        string getName() override { return "Marinara Sauce"; }
+};
+
+class PlumTomatoSauce : public Sauce{
+    public:
+        string getName() override { return "Plum Tomato Sauce"; }
+};
+
+
+class Cheese{
+    public:
+        virtual string getName() = 0;
+        virtual ~Cheese() = default;
+};
+
+class ReggianoCheese : public Cheese{
+    public:
+        virtual string getName(){
+            return "Reggiano Cheese";
         }
 };
 
-class NyPizzaStore : PizzaStore {
+class MozzarellaCheese : public Cheese{
     public:
-        Pizza createPizza(string type) override {
-            if(type == "cheese") return new NyStyledCheesePizza();
+        virtual string getName(){
+            return "Mozzarella Cheese";
         }
 };
 
-class Pizza {
-    public:
-        PizzaIngredientFactory* pizzaIngredientFactory;
-        virtual void bake() = 0;
-        void cut() = 0;
-        virtual void box() = 0;
-
-};
-
-class NyStyledCheesePizza : public Pizza {
-
-};
 
 class PizzaIngredientFactory{
     public:
-        Dough* dough;
-        Sauce* sauce;
+        virtual Dough* createDough() = 0;
+        virtual Sauce* createSauce() = 0;
+        virtual Cheese* createCheese() = 0;
+        virtual ~PizzaIngredientFactory() = default;
 };
 
-class NyPizzaIngredientFactory : PizzaIngredientFactory{
+class NYPizzaIngredientFactory : public PizzaIngredientFactory{
     public:
-        NyPizzaIngredientFactory() {
-            dough = new ThinDough();
-            sauce = new PizzaSauce();
+        Dough* createDough() override { return new ThickCrustDough(); }
+        Sauce* createSauce() override { return new MarinaraSauce(); }
+        Cheese* createCheese() override { return new ReggianoCheese(); }
+};
+
+class ChicagoPizzaIngredientFactory : public PizzaIngredientFactory{
+    public:
+        Dough* createDough() override { return new ThinCrustDough(); }
+        Sauce* createSauce() override { return new PlumTomatoSauce(); }
+        Cheese* createCheese() override { return new MozzarellaCheese(); }
+};
+
+class Pizza{
+    protected:
+        string name;
+        Dough* dough = nullptr;
+        Sauce* sauce = nullptr;
+        Cheese* cheese = nullptr;
+
+    public:
+        virtual void prepare() = 0;
+
+        virtual void bake(){
+            cout << "Baking fro 30 mins...\n";
+        }
+
+        virtual void cut(){
+            cout << "Cutting the pizza\n";
+        }
+
+        virtual void box(){
+            cout << "Boxing the pizza\n";
+        }
+
+        string getName(){
+            return name;
+        }
+
+        virtual ~Pizza(){
+            delete dough;
+            delete sauce;
+            delete cheese;
         }
 };
 
-class Dough {
-    public:
-        virtual void name() = 0;
-};
+class CheesePizza : public Pizza { 
+    PizzaIngredientFactory* pizzaIngredientFactory;
 
-class ThinDough : public Dough {
     public:
-        void name() override {
-            cout << "Thin Dough" << endl;
+        CheesePizza(PizzaIngredientFactory* pifactory) : pizzaIngredientFactory(pifactory){
+            name = "Cheese Pizza";
+        }
+
+        void prepare() override {
+            cout << "Preparing " << name << "\n";
+            dough = pizzaIngredientFactory->createDough();
+            sauce = pizzaIngredientFactory->createSauce();
+            cheese = pizzaIngredientFactory->createCheese();
+
+            cout << " Dough: " << dough->getName() << "\n";
+            cout << " Sauce: " << sauce->getName() << "\n";
+            cout << " Cheese: " << cheese->getName() << "\n";
         }
 };
 
-class Sauce {
-    public:
-        virtual void name() = 0;
-};
+class PepperoniPizza : public Pizza { 
+    PizzaIngredientFactory* pizzaIngredientFactory;
 
-class PizzaSauce : public Sauce {
     public:
-        void name() override {
-            cout << "Pizza Sauce" << endl;
+        PepperoniPizza(PizzaIngredientFactory* pifactory) : pizzaIngredientFactory(pifactory){
+            name = "Cheese Pizza";
+        }
+
+        void prepare() override {
+            cout << "Preparing " << name << "\n";
+            dough = pizzaIngredientFactory->createDough();
+            sauce = pizzaIngredientFactory->createSauce();
+            cheese = pizzaIngredientFactory->createCheese();
+
+            cout << " Dough: " << dough->getName() << "\n";
+            cout << " Sauce: " << sauce->getName() << "\n";
+            cout << " Cheese: " << cheese->getName() << "\n";
         }
 };
+
+class PizzaStore{
+    public:
+        virtual Pizza* createPizza(string pizzaName) = 0;
+
+        Pizza* orderPizza(string pizzaName){
+            Pizza* pizza = createPizza(pizzaName);
+            cout << "Making a " << pizza->getName() << "\n";
+            pizza->prepare();
+            pizza->bake();
+            pizza->cut();
+            pizza->box();
+            return pizza;
+        }
+
+        virtual ~PizzaStore() = default;
+};
+
+class NYPizzaStore : public PizzaStore{
+    NYPizzaIngredientFactory factory;
+    public:
+        Pizza* createPizza(string type) override {
+            if(type == "cheese") return new CheesePizza(&factory);
+            else if(type == "pepperoni") return new PepperoniPizza(&factory);
+            else return nullptr;
+        }
+};
+
+class ChicagoPizzaStore : public PizzaStore{
+    NYPizzaIngredientFactory factory;
+    public:
+        Pizza* createPizza(string type) override {
+            if(type == "cheese") return new CheesePizza(&factory);
+            else if(type == "pepperoni") return new PepperoniPizza(&factory);
+            else return nullptr;
+        }
+};
+
 
 int main(){
-    Dough* thinDough = new ThinDough();
-    thinDough->name();
+    NYPizzaStore nyStore;
+    ChicagoPizzaStore chicagoStore;
+
+    Pizza* p1 = nyStore.orderPizza("cheese");
+    cout << "\n";
+    Pizza* p2 = nyStore.orderPizza("pepperoni");
+    cout << "\n";
+
+    delete p1;
+    delete p2;
+    
     return 0;
 }
